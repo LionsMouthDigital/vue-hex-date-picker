59196a648abf286f0cede2e1f5f9a15f8d8b7e65
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-06-10 12:00:53 -0500

    :stars: add ability to disable days/dates/past/future | :stars: add classes for styling closes LionsMouthDigital/Hex#35

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
+    <h1>Hex Date Picker</h1>
+    <h2>Simple</h2>
     <hex-date-picker v-ref:one></hex-date-picker>
     <input type="text" v-model="$refs.one.selected">
     <input type="text" v-model="$refs.one.displayDate">
 
-    <hex-date-picker picked="2015-12-31" v-ref:two></hex-date-picker>
+    <h2>Default picked date</h2>
+    <hex-date-picker picked="2015-12-30" v-ref:two></hex-date-picker>
     <input type="text" v-model="$refs.two.selected">
     <input type="text" v-model="$refs.two.displayDate">
+
+    <h2>Disabling</h2>
+    <h3>Dates</h3>
+    <hex-date-picker
+      picked        = "2015-12-30"
+      disable-dates = "2015-12-24|2015-12-25|2015-12-31"
+      v-ref:three
+    ></hex-date-picker>
+    <input type="text" v-model="$refs.three.selected">
+    <input type="text" v-model="$refs.three.displayDate">
+
+    <h3>Days of the week</h3>
+    <hex-date-picker disable-days="Saturday|Sunday" v-ref:four></hex-date-picker>
+    <input type="text" v-model="$refs.four.selected">
+    <input type="text" v-model="$refs.four.displayDate">
+
+    <h3>Past</h3>
+    <hex-date-picker disable-past v-ref:five></hex-date-picker>
+    <input type="text" v-model="$refs.five.selected">
+    <input type="text" v-model="$refs.five.displayDate">
+
+    <h3>Future</h3>
+    <hex-date-picker disable-future v-ref:six></hex-date-picker>
+    <input type="text" v-model="$refs.six.selected">
+    <input type="text" v-model="$refs.six.displayDate">
   </div>
 </template>
 
--- src/components/HexDatePicker.vue
+++ src/components/HexDatePicker.vue
@@ -2,40 +2,36 @@
   <div class="calendar">
     <table>
       <thead>
-        <tr>
+        <tr class="controls">
           <th>
-            <button @click="prevMonth()" class="button-prev">
-              <span>&lt;</span>
-            </button>
+            <button @click="prevMonth()" class="button-prev"></button>
           </th>
           <th colspan="2">{{ moment(date).format('MMM') }}</th>
           <th>
-            <button @click="nextMonth()" class="button-next">
-              <span>&gt;</span>
-            </button>
+            <button @click="nextMonth()" class="button-next"></button>
           </th>
 
           <th>
-            <button @click="prevYear()" class="button-prev">
-              <span>&lt;</span>
-            </button>
+            <button @click="prevYear()" class="button-prev"></button>
           </th>
           <th>{{ moment(date).format('YYYY') }}</th>
           <th>
-            <button @click="nextYear()" class="button-next">
-              <span>&gt;</span>
-            </button>
+            <button @click="nextYear()" class="button-next"></button>
           </th>
         </tr>
 
-        <tr>
+        <tr class="days-of-week">
           <th v-for="day in daysOfWeek">{{ day }}</th>
         </tr>
       </thead>
 
       <tbody>
         <tr v-for="week in weeks">
-          <td v-for="day in week" @click="pick(day)">
+          <td
+            v-for  = "day in week"
+            @click = "pick(day)"
+            class  = "{{ getDateClasses(day) }}"
+          >
             {{ moment(day, format).format('DD') }}
           </td>
         </tr>
@@ -45,7 +41,7 @@
 </template>
 
 <script>
-  import moment from 'moment';
+  import moment      from 'moment';
   import HexCalendar from 'vue-hex-calendar';
 
   export default {
@@ -55,9 +51,12 @@
       return {
         // Base date to build the calendar on. If `picked` prop is unspecified, it will default to
         // today, and the rest of the current month will be shown.
-        date:     '',
+        date:          '',
+        disabled:      '',
+        formatPrecise: 'YYYY-MM-DD HH:mm',
         // Selected date.
-        selected: '',
+        selected:      '',
+        today:         moment().format(this.format),
       };
     },
 
@@ -76,6 +75,15 @@
 
 
     props: {
+      // Prevent dates from getting picked.
+      // Accepts a pipe-separated list of dates in `YYYY-MM-DD` format.
+      disableDates:  String,
+      // Accepts a pipe-separated list of days of the week.
+      disableDays:   String,
+      disablePast:   Boolean,
+      disableToday:  Boolean,
+      disableFuture: Boolean,
+
       // Set the user-friendly date format.
       // See [Moment.js docs](http://momentjs.com/docs/#year-month-and-day-tokens).
       displayForamt: {
@@ -84,11 +92,59 @@
       },
 
       // Set the default selected date.
-      picked: String
+      picked: String,
     },
 
 
     methods: {
+      /**
+       * Get appropriate classes for a given date.
+       *
+       * @author Curtis Blackwell
+       * @param  {string} date Date to get classes for.
+       * @return {string}
+       */
+      getDateClasses(date) {
+        var classes = '';
+        // For today.
+        if (this.today === date) {
+          classes+= ' today';
+        }
+        // For currently picked date.
+        if (date === this.selected) {
+          classes+= ' picked';
+        }
+        // For disabled dates.
+        if (this.isDisabled(date)) {
+          classes+= ' disabled';
+        }
+        // For days in inactive months.
+        if ((this.date).split('-')[1] !== date.split('-')[1]) {
+          classes+= ' in-inactive-month';
+        }
+
+        return classes;
+      },
+
+      /**
+       * Determine whether a given date is disabled.
+       *
+       * @author Curtis Blackwell
+       * @param  {string}  date Date to check.
+       * @return {Boolean}
+       */
+      isDisabled(date) {
+        var datePrecise   = moment(date + ' 00:00', this.formatPrecise);
+        var todayPrecise  = moment(moment().format(this.format) + ' 00:00', this.formatPrecise);
+        var diffFromToday = datePrecise.diff(todayPrecise, 'days');
+
+        return this.disabled.indexOf(date) > -1                        ||
+               this.disabled.indexOf(moment(date).format('dddd')) > -1 ||
+               this.disablePast   && diffFromToday < 0                 ||
+               this.disableToday  && date === this.today               ||
+               this.disableFuture && diffFromToday > 0;
+      },
+
       /**
        * Select a date.
        *
@@ -97,15 +153,22 @@
        * @return {void}
        */
       pick(date) {
-        this.selected = date;
+        // Ensure the date isn't disabled and select it.
+        this.selected = !this.isDisabled(date) ? date : this.selected;
       },
     },
 
 
     ready() {
       // Set the date based on the `picked` prop.
-      this.date     = this.picked ? this.picked : moment();
+      this.date     = this.picked ? this.picked : moment().format(this.format);
       this.selected = this.picked;
+
+      // Create an array of disabled dates based on the `disable` prop.
+      var disabledDays  = typeof this.disableDays  === 'string' ? this.disableDays.split('|')  : [];
+      var disabledDates = typeof this.disableDates === 'string' ? this.disableDates.split('|') : [];
+
+      this.disabled = disabledDays.concat(disabledDates);
     },
   };
 </script>
